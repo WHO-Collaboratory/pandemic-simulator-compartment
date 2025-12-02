c7f87282ea78647a99b973300fe1605b64d080ed
Daniel Langford <dmlangford14@gmail.com>
2025-12-02 17:11:53 -0500

    remove local cloud testing

--- compartment/examples/covid_jax_model/main.py
+++ compartment/examples/covid_jax_model/main.py
@@ -6,9 +6,7 @@ import time
 import tracemalloc
 from datetime import datetime
 from compartment.batch_helpers.simulation_helpers import get_simulation_params
-
 from compartment.run_simulation import run_simulation
-from compartment.examples.covid_jax_model.model import CovidJaxModel
 
 logging.getLogger("jax").setLevel(logging.WARNING)
 logging.getLogger("jax._src").setLevel(logging.WARNING)
@@ -38,13 +36,7 @@ if __name__ == "__main__":
             output_file = f"{config_basename}-results-{timestamp}.json"
         run_metadata = run_simulation(config_path=args.config_file, output_path=output_file)
     else:
-        #simulation_params = get_simulation_params()
-        simulation_params = {
-            "SIMULATION_JOB_ID": "4c8efee7-7310-452e-ba76-1788e69f46c2",#"fb1eab7d-73cb-4c1e-a0c6-16b53600fdb7",#covid:"9814c00a-98f7-4726-a8a9-fa30cb9572de",#"9c9d1052-cfcb-4edc-8b54-3177bf678c97",#"dc60bf28-b4b5-40c1-81bd-09f304b67190",
-            "GRAPHQL_ENDPOINT": "https://ftvz6ss74ncwxnn2a4a6dia664.appsync-api.us-east-1.amazonaws.com/graphql",
-            "GRAPHQL_APIKEY": "da2-4pf5tbnxgfddlel57zztt6id4y",
-            "ENVIRONMENT": "dev"
-        }
+        simulation_params = get_simulation_params()
         run_metadata = run_simulation(simulation_params=simulation_params)
 
     logger.info(f"run_metadata: {run_metadata}")
