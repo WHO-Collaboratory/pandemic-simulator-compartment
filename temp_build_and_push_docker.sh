ea6c244869ab5fc6946a3ec646b62bb85521da0c
Daniel Langford <dmlangford14@gmail.com>
2026-01-13 12:31:08 -0500

    feat: tpu testing

---
+++ temp_build_and_push_docker.sh
@@ -0,0 +1,10 @@
+# 1. Define your variables for easy updates
+export PROJECT_ID=$(gcloud config get-value project)
+export REPO_PATH="us-east1-docker.pkg.dev/${PROJECT_ID}/simulations/covid-jax-tpu"
+
+# 2. Build the image for the correct architecture (linux/amd64)
+# We use --platform to ensure it runs correctly on the GCP TPU nodes
+docker build --platform linux/amd64 -t $REPO_PATH:latest .
+
+# 3. Push the image to the cloud
+docker push $REPO_PATH:latest
