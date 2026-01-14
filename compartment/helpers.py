a3fc00985d13da5ae6f3701b1bcea165bf6c7ccd
Jenny Blase <jblase@ruvos.com>
2026-01-14 16:51:21 -0500

    Change id to name for matrix operations

--- compartment/helpers.py
+++ compartment/helpers.py
@@ -730,7 +730,7 @@ def prepare_covid_initial_state(initial_population, age_transmission, demographi
 # --------------------------------------------------
 
 def get_admin_zone_df(case_file):
-    ll_pop = pd.DataFrame(case_file, columns=["id", "center_lat", "center_lon", "population"])
+    ll_pop = pd.DataFrame(case_file, columns=["name", "center_lat", "center_lon", "population"])
     ll_pop['lat_long'] = list(zip(ll_pop.center_lat, ll_pop.center_lon))
     ll_pop.drop(columns=['center_lat', 'center_lon'], inplace=True)
 
@@ -766,10 +766,10 @@ def create_travel_matrix(input_df, sigma):
     input_df = gravity_model(input_df, 'population_origin', 'population_destination', 'distance_km')
     input_df['gravity'] = input_df['gravity'].replace(np.inf, 0)
     # Calculate a rate, e.g., flow per origin
-    input_df["gravity_rate"] = input_df["gravity"] / input_df.groupby("id_origin")["gravity"].transform("sum")
+    input_df["gravity_rate"] = input_df["gravity"] / input_df.groupby("name_origin")["gravity"].transform("sum")
     pivot_df = pd.pivot_table(input_df, 
-                            index='id_origin', 
-                            columns='id_destination', 
+                            index='name_origin', 
+                            columns='name_destination', 
                             values='gravity_rate', 
                             aggfunc='sum')
     travel_matrix = pivot_df.T
