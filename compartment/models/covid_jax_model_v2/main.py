eb43cd1e87c8ce38e2fdd843c07e3add16b9f3df
Jordan <jhansen@ruvos.com>
2026-03-13 00:59:20 -0400

    [WIP] Add auto-compute of transmission_edges w/ escape hatch for manual math

---
+++ compartment/models/covid_jax_model_v2/main.py
@@ -0,0 +1,43 @@
+import logging
+import argparse
+from compartment.driver import drive_simulation
+from compartment.models.covid_jax_model_v2.model import CovidJaxModelV2
+
+logging.getLogger("jax").setLevel(logging.WARNING)
+logging.getLogger("jax._src").setLevel(logging.WARNING)
+logging.getLogger("jax._src.xla_bridge").setLevel(logging.WARNING)
+logger = logging.getLogger()
+
+
+def lambda_handler(event, context):
+    drive_simulation(
+        model_class=CovidJaxModelV2,
+        args={"mode": "cloud", "simulation_job_id": event["simulation_job_id"]},
+    )
+    return None
+
+
+if __name__ == "__main__":
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description="Run pandemic simulation with specified config file"
+    )
+    parser.add_argument("--mode", choices=["local", "cloud"], default="local")
+    parser.add_argument(
+        "--config_file", help="Path to the JSON config file for the simulation"
+    )
+    parser.add_argument(
+        "--output_file",
+        nargs="?",
+        default=None,
+        help="Pass a relative path from your execution directory. Use * to generate a default output filename with timestamp.",
+    )
+    parser.add_argument(
+        "--simulation_job_id",
+        nargs="?",
+        default=None,
+        help="Existing simulation job id in a graphql backend.",
+    )
+    args = parser.parse_args()
+
+    drive_simulation(model_class=CovidJaxModelV2, args=vars(args))
