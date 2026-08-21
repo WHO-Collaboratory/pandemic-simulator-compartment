a27aebc5f2cbc16f6980a39726a1f272c19359a0
jenny-blase-ruvos <110993778+jenny-blase-ruvos@users.noreply.github.com>
2026-08-21 15:31:23 -0400

    Remove AGENT_BASED as option for simulations (#127)

--- compartment/validation/base_simulation.py
+++ compartment/validation/base_simulation.py
@@ -74,7 +74,7 @@ class BaseSimulationShared(BaseModel):
 
     # Owner & meta
     owner: Optional[str] = None
-    simulation_type: Literal["COMPARTMENTAL", "AGENT_BASED"] = "COMPARTMENTAL"
+    simulation_type: Literal["COMPARTMENTAL"] = "COMPARTMENTAL"
     run_mode: Literal["UNCERTAINTY", "DETERMINISTIC", "STOCHASTIC"] = "DETERMINISTIC"
 
     # Time
