57931029eb00e149cd763c01d72af28ffe61fd1d
laiyingsilaiyingsi <laiyingsilaiyingsi@gmail.com>
2026-08-26 21:51:44 +0200

    Add ebola SIR stochastic model

---
+++ compartment/models/ebola_sir_stochastic_lai_v1_model/main.py
@@ -0,0 +1,39 @@
+import logging
+import argparse
+from compartment.driver import drive_simulation
+from compartment.models.ebola_sir_stochastic_lai_v1_model.model import EbolaTestStochasticModel
+
+logging.getLogger("jax").setLevel(logging.WARNING)
+logging.getLogger("jax._src").setLevel(logging.WARNING)
+logging.getLogger("jax._src.xla_bridge").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+
+
+def lambda_handler(event, context):
+    drive_simulation(
+        model_class=EbolaTestStochasticModel,
+        args={"mode": "cloud", "simulation_job_id": event["simulation_job_id"]},
+    )
+    return None
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Run Ebola (SEIHFR, Community/Hospital/Burial) — Legrand 2007 [stochastic] simulation with specified config file"
+    )
+    parser.add_argument("--mode", choices=["local", "cloud"], default="local")
+    parser.add_argument("--config_file", help="Path to the JSON config file for the simulation")
+    parser.add_argument(
+        "--output_file",
+        nargs="?",
+        default=None,
+        help="Output file path. Use * to generate a default filename with timestamp.",
+    )
+    parser.add_argument(
+        "--simulation_job_id",
+        nargs="?",
+        default=None,
+        help="Existing simulation job id in a graphql backend.",
+    )
+    args = parser.parse_args()
+    drive_simulation(model_class=EbolaTestStochasticModel, args=vars(args))
